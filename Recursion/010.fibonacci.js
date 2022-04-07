2a60f1b3119ab48560ed26c395b1e256d49d3c76
Dileep <98578797+iamdileep89@users.noreply.github.com>
2022-04-07 18:10:27 +0000

    more problems

--- Recursion/010.fibonacci.js
+++ Recursion/010.fibonacci.js
@@ -1,3 +1,7 @@
+// find the nth fibonacci number
+
+// The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
 function fib(n){
     if(n<=2) return 1
     return fib(n-1)+fib(n-2)
